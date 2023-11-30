2fc824bc2691336fd9f295be1999f13f1af1d841
Doug Guthrie <doug.guthrie@dbtlabs.com>
2023-11-30 16:26:49 +0000

    Another change

--- models/staging/segment/int_segment__pages.sql
+++ models/staging/segment/int_segment__pages.sql
@@ -28,6 +28,7 @@ with
         context_user_agent,
         '{{ source }}' as src
 
+
     from {{ ref('stg_' ~ source ~ '__pages') }}
     where url not like 'http://127.0.0.1:8000%'
         and url not like 'http://localhost:8000%'
