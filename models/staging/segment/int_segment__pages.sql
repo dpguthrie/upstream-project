4c66ba92e0aff8daa008ee31b2ac601fbfecb0f6
Doug Guthrie <doug.guthrie@dbtlabs.com>
2024-01-18 21:53:39 +0000

    why isn't this working

--- models/staging/segment/int_segment__pages.sql
+++ models/staging/segment/int_segment__pages.sql
@@ -27,6 +27,7 @@ with
         context_page_url,
         context_user_agent,
         '{{ source }}' as src2
+        
 
     from {{ ref('stg_' ~ source ~ '__pages') }}
     where url not like 'http://127.0.0.1:8000%'
