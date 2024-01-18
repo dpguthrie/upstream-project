7811dc4a694dc32f1ed16f1d690c1e3b94cfdbed
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-01-18 16:00:08 -0700

    Switch bad models

--- models/staging/segment/int_segment__pages.sql
+++ models/staging/segment/int_segment__pages.sql
@@ -26,7 +26,7 @@ with
         context_page_path,
         context_page_url,
         context_user_agent,
-        '{{ source }}' as src2
+        '{{ source }}' as src
         
 
     from {{ ref('stg_' ~ source ~ '__pages') }}
